820e9acf0db442e7f8fc215b0da4012ece2c2ea9
Martin Zlámal <mrtnzlml@gmail.com>
2017-03-04 23:48:13 +0100

    Introduce adeira/connector-stream

---
+++ bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+$configurator->setDebugMode(getenv('NETTE_DEBUG') === '1');
+$configurator->enableTracy(__DIR__ . '/var/log');
+
+$configurator->setTimeZone('Europe/Prague');
+$configurator->setTempDirectory(__DIR__ . '/var/temp');
+
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+$requestFactory = new Nette\Http\RequestFactory;
+$requestFactory->urlFilters = [
+	'path' => [
+		'~/{2,}~' => '/', // clean the path from duplicated slashes
+	],
+	'url' => [
+		'~[.,)]$~' => '', // remove dot, comma or right parenthesis form the end of the URL
+	],
+];
+$configurator->addServices([
+	'http.request' => $requestFactory->createHttpRequest(),
+]);
+
+return $configurator->createContainer();
